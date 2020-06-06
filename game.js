d7ab0e3996522685f1287572b828dfaa1285c331
Brian Han <bmhan319@hotmail.com>
2020-06-06 14:46:18 -0700

    added in death

--- game.js
+++ game.js
@@ -1,16 +1,24 @@
-import { SNAKE_SPEED, update as updateSnake, draw as drawSnake }  from './snake.js'
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection }  from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
 
 let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.querySelector('#game-board')
 
 function main(currentTime) {
+  if (gameOver) {
+    if ( confirm("You lost!  Press OK to restart.") ) {
+      location.reload()
+    } 
+    return
+  }
+
   const secondSinceLastRender = (currentTime - lastRenderTime) / 1000
 
   window.requestAnimationFrame(main) 
   if (secondSinceLastRender < 1 / SNAKE_SPEED) return
 
-  console.log("render")
   //console.log(currentTime)
   //console.log(lastRenderTime)
   //console.log(secondSinceLastRender)
@@ -29,6 +37,7 @@ window.requestAnimationFrame(main)
 function update() {
   updateSnake()
   updateFood()
+  checkDeath()
 }
 
 function draw() {
@@ -36,4 +45,8 @@ function draw() {
   gameBoard.innerHTML = ''
   drawSnake(gameBoard)
   drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outsideGrid( getSnakeHead() ) || snakeIntersection()
 }
